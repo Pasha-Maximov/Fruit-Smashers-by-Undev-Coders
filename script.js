5194b35cdb2c0af0bab3f938917c4911d189cc86
Pasha-Maximov <167475063+Pasha-Maximov@users.noreply.github.com>
2024-09-17 16:18:14 -0400

    g

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
 const canvas = document.getElementById('gameCanvas');
-const canvas2 = document.getElementById('gameCanvas2');
-const canvas3 = document.getElementById('gameCanvas3');
-const canvas4 = document.getElementById('gameCanvas4');
-const canvas5 = document.getElementById('gameCanvas5');
-const canvas6 = document.getElementById('gameCanvas6');
-const canvas7 = document.getElementById('gameCanvas7');
-const canvas8 = document.getElementById('gameCanvas8');
+
 const ctx = canvas.getContext('2d');
-const ctx2 = canvas2.getContext('2d');
-const ctx3 = canvas3.getContext('2d');
-const ctx4 = canvas4.getContext('2d');
-const ctx5 = canvas5.getContext('2d');
-const ctx6 = canvas6.getContext('2d');
-const ctx7 = canvas7.getContext('2d');
-const ctx8 = canvas8.getContext('2d');
+
 
 const WIDTH = 1115;
 const HEIGHT = 650;
@@ -21,34 +9,20 @@ canvas.width = WIDTH;
 canvas.height = HEIGHT;
 const WIDTH2 = 350;
 const HEIGHT2 = 600;
-canvas2.width = WIDTH2;
-canvas2.height = HEIGHT2;
-const WIDTH3 = 350;
-const HEIGHT3 = 600;
-canvas3.width = WIDTH3;
-canvas3.height = HEIGHT3;
-const WIDTH4 = 350;
-const HEIGHT4 = 600;
-canvas4.width = WIDTH4;
-canvas4.height = HEIGHT4;
-const WIDTH5 = 150;
-const HEIGHT5 = 550;
-canvas5.width = WIDTH5;
-canvas5.height = HEIGHT5;
-const WIDTH6 = 150;
-const HEIGHT6 = 550;
-canvas6.width = WIDTH6;
-canvas6.height = HEIGHT6;
-const WIDTH7 = 300;
-const HEIGHT7 = 250;
-canvas7.width = WIDTH7;
-canvas7.height = HEIGHT7;
-const WIDTH8 = 300;
-const HEIGHT8 = 250;
-canvas8.width = WIDTH8;
-canvas8.height = HEIGHT8;
 
 
+
+
+let subcanvas = []
+
+
+
+function drawsubcanvas() {
+    subcanvas.forEach((subcanvas) => {
+        ctx.fillStyle = 'white';
+        ctx.fillRect(subcanvas.x, subcanvas.y, subcanvas.width, subcanvas.height);
+    });
+}
 
 
 
