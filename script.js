2d43d7eef13aa62cd01eb281eeb5fe57ccb91801
Pasha-Maximov <167475063+Pasha-Maximov@users.noreply.github.com>
2024-09-21 16:34:36 -0400

    h

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const canvas = document.getElementById('gameCanvas');
-
 const ctx = canvas.getContext('2d');
 
 
@@ -7,8 +6,229 @@ const WIDTH = 1115;
 const HEIGHT = 650;
 canvas.width = WIDTH;
 canvas.height = HEIGHT;
-const WIDTH2 = 350;
-const HEIGHT2 = 600;
+
+const bowlphase0 = new Image();
+bowlphase0.src = 'bowl_phase_0-removebg-preview.pngghtning.png';
+
+const bowlphase1 = new Image();
+bowlphase1.src = 'bowl_phase_1-removebg-preview.pngghtning.png';
+
+
+
+ctx.fillStyle = 'green';
+ctx.fillRect(400, 25, 315, 600);
+
+ctx.fillStyle = 'green';
+ctx.fillRect(757.5, 25, 315, 600);
+
+ctx.fillStyle = 'green';
+ctx.fillRect(42.5, 25, 315, 600);
+
+ctx.fillStyle = 'rgba(0, 0, 0, 0.5';
+ctx.fillRect(770, 50, 140, 560);
+
+ctx.fillStyle = 'rgba(0, 0, 0, 0.5';
+ctx.fillRect(920, 50, 140, 560);
+
+ctx.fillStyle = 'rgba(0, 0, 0, 0.5';
+ctx.fillRect(413, 50, 290, 280);
+
+ctx.fillStyle = 'rgba(0, 0, 0, 0.5';
+ctx.fillRect(413, 355, 290, 260);
+
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(425, 360, 4, 54);
+ctx.fillRect(425, 360, 54, 4);
+ctx.fillRect(475, 360, 4, 54);
+ctx.fillRect(425, 410, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(495, 360, 4, 54);
+ctx.fillRect(495, 360, 54, 4);
+ctx.fillRect(545, 360, 4, 54);
+ctx.fillRect(495, 410, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(565, 360, 4, 54);
+ctx.fillRect(565, 360, 54, 4);
+ctx.fillRect(615, 360, 4, 54);
+ctx.fillRect(565, 410, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(635, 360, 4, 54);
+ctx.fillRect(635, 360, 54, 4);
+ctx.fillRect(685, 360, 4, 54);
+ctx.fillRect(635, 410, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(425, 430, 4, 54);
+ctx.fillRect(425, 430, 54, 4);
+ctx.fillRect(475, 430, 4, 54);
+ctx.fillRect(425, 480, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(495, 430, 4, 54);
+ctx.fillRect(495, 430, 54, 4);
+ctx.fillRect(545, 430, 4, 54);
+ctx.fillRect(495, 480, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(565, 430, 4, 54);
+ctx.fillRect(565, 430, 54, 4);
+ctx.fillRect(615, 430, 4, 54);
+ctx.fillRect(565, 480, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(635, 430, 4, 54);
+ctx.fillRect(635, 430, 54, 4);
+ctx.fillRect(685, 430, 4, 54);
+ctx.fillRect(635, 480, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(425, 500, 4, 54);
+ctx.fillRect(425, 500, 54, 4);
+ctx.fillRect(475, 500, 4, 54);
+ctx.fillRect(425, 550, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(495, 500, 4, 54);
+ctx.fillRect(495, 500, 54, 4);
+ctx.fillRect(545, 500, 4, 54);
+ctx.fillRect(495, 550, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(565, 500, 4, 54);
+ctx.fillRect(565, 500, 54, 4);
+ctx.fillRect(615, 500, 4, 54);
+ctx.fillRect(565, 550, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(635, 500, 4, 54);
+ctx.fillRect(635, 500, 54, 4);
+ctx.fillRect(685, 500, 4, 54);
+ctx.fillRect(635, 550, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(425, 570, 4, 54);
+ctx.fillRect(425, 570, 54, 4);
+ctx.fillRect(475, 570, 4, 54);
+ctx.fillRect(425, 620, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(495, 570, 4, 54);
+ctx.fillRect(495, 570, 54, 4);
+ctx.fillRect(545, 570, 4, 54);
+ctx.fillRect(495, 620, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(565, 570, 4, 54);
+ctx.fillRect(565, 570, 54, 4);
+ctx.fillRect(615, 570, 4, 54);
+ctx.fillRect(565, 620, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(635, 570, 4, 54);
+ctx.fillRect(635, 570, 54, 4);
+ctx.fillRect(685, 570, 4, 54);
+ctx.fillRect(635, 620, 54, 4);
+//------------------------------------------------------------------------------//
+//shop//
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(425, 55, 4, 54);
+ctx.fillRect(425, 55, 54, 4);
+ctx.fillRect(475, 55, 4, 54);
+ctx.fillRect(425, 105, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(495, 55, 4, 54);
+ctx.fillRect(495, 55, 54, 4);
+ctx.fillRect(545, 55, 4, 54);
+ctx.fillRect(495, 105, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(565, 55, 4, 54);
+ctx.fillRect(565, 55, 54, 4);
+ctx.fillRect(615, 55, 4, 54);
+ctx.fillRect(565, 105, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(635, 55, 4, 54);
+ctx.fillRect(635, 55, 54, 4);
+ctx.fillRect(685, 55, 4, 54);
+ctx.fillRect(635, 105, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(425, 125, 4, 54);
+ctx.fillRect(425, 125, 54, 4);
+ctx.fillRect(475, 125, 4, 54);
+ctx.fillRect(425, 175, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(495, 430, 4, 54);
+ctx.fillRect(495, 430, 54, 4);
+ctx.fillRect(545, 430, 4, 54);
+ctx.fillRect(495, 480, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(565, 430, 4, 54);
+ctx.fillRect(565, 430, 54, 4);
+ctx.fillRect(615, 430, 4, 54);
+ctx.fillRect(565, 480, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(635, 430, 4, 54);
+ctx.fillRect(635, 430, 54, 4);
+ctx.fillRect(685, 430, 4, 54);
+ctx.fillRect(635, 480, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(425, 500, 4, 54);
+ctx.fillRect(425, 500, 54, 4);
+ctx.fillRect(475, 500, 4, 54);
+ctx.fillRect(425, 550, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(495, 500, 4, 54);
+ctx.fillRect(495, 500, 54, 4);
+ctx.fillRect(545, 500, 4, 54);
+ctx.fillRect(495, 550, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(565, 500, 4, 54);
+ctx.fillRect(565, 500, 54, 4);
+ctx.fillRect(615, 500, 4, 54);
+ctx.fillRect(565, 550, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(635, 500, 4, 54);
+ctx.fillRect(635, 500, 54, 4);
+ctx.fillRect(685, 500, 4, 54);
+ctx.fillRect(635, 550, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(425, 570, 4, 54);
+ctx.fillRect(425, 570, 54, 4);
+ctx.fillRect(475, 570, 4, 54);
+ctx.fillRect(425, 620, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(495, 570, 4, 54);
+ctx.fillRect(495, 570, 54, 4);
+ctx.fillRect(545, 570, 4, 54);
+ctx.fillRect(495, 620, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(565, 570, 4, 54);
+ctx.fillRect(565, 570, 54, 4);
+ctx.fillRect(615, 570, 4, 54);
+ctx.fillRect(565, 620, 54, 4);
+
+ctx.fillStyle = 'rgba(200, 200, 200, 1';
+ctx.fillRect(635, 570, 4, 54);
+ctx.fillRect(635, 570, 54, 4);
+ctx.fillRect(685, 570, 4, 54);
+ctx.fillRect(635, 620, 54, 4);
 
 
 
@@ -17,5 +237,9 @@ const HEIGHT2 = 600;
 
 
 
+ctx.fillStyle = 'white'
+ctx.font= '25px bold arial'
+ctx.fillText('Achievements', 485, 350);
 
 
+ctx.drawimage(bowlphase0, 200, 100, 20, 20)
